e607693761edcc8ec1f338fb9991f4027858d09a
Lukas Warren <lukaswarren728@gmail.com>
2021-02-15 11:37:02 -0700

    tweak

--- movieviewer/app/service/movie-by-title.js
+++ movieviewer/app/service/movie-by-title.js
@@ -1,6 +1,6 @@
 const superagent = require('superagent');
 module.exports = {
-    movietitle:  async function getMovieByTitle(title){
+    movietitle: function getMovieByTitle(title){
         return superagent.get('https://api.themoviedb.org/3/search/movie')
                 .query({api_key: '217924914d84ddfc0b4606bda0560d6b', language : 'en-US', query : title})
                 .then(response =>{
